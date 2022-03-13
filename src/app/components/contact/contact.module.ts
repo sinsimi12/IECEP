5167a9dbd9641fb32a83515cdc21dc94e5e3619f
Christian Mina <christian.mina@tup.edu.ph>
2022-03-13 10:35:54 +0800

    Add import modules.

---
+++ src/app/components/contact/contact.module.ts
@@ -0,0 +1,5 @@
+//Module
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
