2dbda104491ae745f9d214f0cb2845fd486f3d1c
Christian Mina <christian.mina@tup.edu.ph>
2022-03-13 10:36:16 +0800

    Add contact component imports.

--- src/app/components/contact/contact.module.ts
+++ src/app/components/contact/contact.module.ts
@@ -3,3 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+
+//Components
+
+import { ContactComponent } from './contact.component';
